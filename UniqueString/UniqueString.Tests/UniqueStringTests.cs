34e96826126ce49e6aaa207d2ec727f0545e056c
James Lieu <james.lieu@kineticsoftware.com>
2021-11-23 23:55:45 +0000

    [Add] UniqueString

---
+++ UniqueString/UniqueString.Tests/UniqueStringTests.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+
+namespace UniqueString.Tests
+{
+    public class Tests
+    {
+        [Test]
+        [TestCase(null, false)]
+        [TestCase("", false)]
+        [TestCase("chromee", false)]
+        [TestCase("chrromee", false)]
+        [TestCase("c", true)]
+        [TestCase("chrome", true)]
+        [TestCase("chr0mE", true)]
+
+        public void Test_IsUnique1(string str, bool expected)
+        {
+            var actual = UniqueString.IsUnique1(str);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(null, false)]
+        [TestCase("", false)]
+        [TestCase("chromee", false)]
+        [TestCase("chrromee", false)]
+        [TestCase("c", true)]
+        [TestCase("chrome", true)]
+        [TestCase("chr0mE", true)]
+
+        public void Test_IsUnique2(string str, bool expected)
+        {
+            var actual = UniqueString.IsUnique2(str);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ UniqueString/UniqueString/UniqueString.cs
@@ -0,0 +1,45 @@
+﻿namespace UniqueString
+{
+    // Implement an algorithm to determine if a string as all unique characters.
+    public class UniqueString
+    {
+        public static bool IsUnique1(string str)
+        {
+            // Handle null or empty cases
+            if (string.IsNullOrEmpty(str)) { return false; }
+
+            // Store the ones we've seen.
+            // Check the list to see if the character already exists 
+            var characterSet = new List<char>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (characterSet.Contains(str[i])) { return false; }
+                characterSet.Add(str[i]);
+            }
+            return true;
+        }
+
+       // What if we can't use additional data structures?
+        public static bool IsUnique2(string str)
+        {
+            // Handle null or empty cases
+            if (string.IsNullOrEmpty(str)) { return false; }
+
+            // Lets assume that the string will always be an ASCII string instead of unicode
+            // There are 128 unique characters in ascii(OR 256 if extended ascii)
+            if (str.Length > 128) { return false; }
+
+            // Create an array of booleans and set the ascii value of the character at index i to true.
+            // The next time we see character i then immediately return false
+            var characterSet = new bool[128];
+            for (int i = 0; i < str.Length; i++)
+            {
+                var val = ((int)str[i]);
+                if (characterSet[val]) { return false; }
+                characterSet[val] = true;
+            };
+            return true;
+        }
+    }
+}
